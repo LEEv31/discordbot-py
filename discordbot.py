7fc2094b69f5eb206b874af993986da882415ce7
LEEv31 <121157749+LEEv31@users.noreply.github.com>
2022-12-21 19:31:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,65 +19,69 @@ mmm = ""
 
 @bot.command()
 async def 레이드(ctx, *, message=None):
-    global a
-    a = message
-    global abc
-    global msg
-    abc = Embed(title="레이드 길드 팟", description=a)
-    abc.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-    abc.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-    msg = await ctx.send(embed=abc)
-    await msg.add_reaction("1️⃣")
-    await msg.add_reaction("2️⃣")
-    await msg.add_reaction("⚠️")
+    if ctx.message.author.guild_permissions.administrator:
+        global a
+        a = message
+        global abc
+        global msg
+        abc = Embed(title="레이드 길드 팟", description=a)
+        abc.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+        abc.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+        msg = await ctx.send(embed=abc)
+        await msg.add_reaction("1️⃣")
+        await msg.add_reaction("2️⃣")
+        await msg.add_reaction("⚠️")
     
 @bot.command()
 async def 레이드수정(ctx, *, mmm=None):
-    global msg
-    a=mmm
-    abv = Embed(title="레이드 길드 팟", description=a)
-    abv.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-    abv.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-    await msg.edit(embed=abv)
+    if ctx.message.author.guild_permissions.administrator:
+        global msg
+        a=mmm
+        abv = Embed(title="레이드 길드 팟", description=a)
+        abv.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+        abv.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+        await msg.edit(embed=abv)
     
 @bot.event
 async def on_reaction_add(reaction, user):
-    global msg
-    if user.bot == 1:
-        return None
-    if str(reaction.emoji) == "1️⃣":
-            x.insert(i,user.name)
-            abd = Embed(title="레이드 길드 팟", description=a)
-            abd.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-            abd.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-            await msg.edit(embed=abd)
-            i+1
-          
-    if str(reaction.emoji) == "2️⃣":
-            y.insert(k,user.name)
-            abg = Embed(title="레이드 길드 팟", description=a)
-            abg.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-            abg.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-            await msg.edit(embed=abg)
-            k+1
-          
-    if str(reaction.emoji) == "⚠️":
-        embed = Embed(title="⚠️길드팟 모집 완료⚠️")
-        await reaction.message.channel.send(embed=embed)
+    if ctx.message.author.guild_permissions.administrator:
+        global msg
+        if user.bot == 1:
+            return None
+        if str(reaction.emoji) == "1️⃣":
+                x.insert(i,user.name)
+                abd = Embed(title="레이드 길드 팟", description=a)
+                abd.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+                abd.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+                await msg.edit(embed=abd)
+                i+1
+
+        if str(reaction.emoji) == "2️⃣":
+                y.insert(k,user.name)
+                abg = Embed(title="레이드 길드 팟", description=a)
+                abg.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+                abg.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+                await msg.edit(embed=abg)
+                k+1
+
+        if str(reaction.emoji) == "⚠️":
+            embed = Embed(title="⚠️길드팟 모집 완료⚠️")
+            await reaction.message.channel.send(embed=embed)
 @bot.event
 async def on_reaction_remove(reaction, user):
-    global msg
-    if str(reaction.emoji) == "1️⃣":
-            x.remove(user.name)
-            abe = Embed(title="레이드 길드 팟", description=a)
-            abe.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-            abe.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-            await msg.edit(embed=abe)
-    if str(reaction.emoji) == "2️⃣":
-            y.remove(user.name)
-            abf = Embed(title="레이드 길드 팟", description=a)
-            abf.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-            abf.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-            await msg.edit(embed=abf)
-            
+    if ctx.message.author.guild_permissions.administrator:
+        global msg
+        if str(reaction.emoji) == "1️⃣":
+                x.remove(user.name)
+                abe = Embed(title="레이드 길드 팟", description=a)
+                abe.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+                abe.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+                await msg.edit(embed=abe)
+        if str(reaction.emoji) == "2️⃣":
+                y.remove(user.name)
+                abf = Embed(title="레이드 길드 팟", description=a)
+                abf.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+                abf.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+                await msg.edit(embed=abf)
+
 bot.run(TOKEN)
