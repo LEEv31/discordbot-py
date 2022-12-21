922bb11fb6e153deab52ef6f7d16bd6179fce114
LEEv31 <121157749+LEEv31@users.noreply.github.com>
2022-12-21 18:16:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ intents = discord.Intents.all()
 
 TOKEN = os.environ['TOKEN']
 PREFIX = os.environ['PREFIX']
-client = client.Bot(intents=intents)
+client = (intents=intents)
 
 x = ["X","X","X","X","X"]
 y = ["X","X","X","X","X"]
