edaf1fbc63eb5624a9976f9985912e8b1aef1157
LEEv31 <121157749+LEEv31@users.noreply.github.com>
2022-12-29 03:10:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -239,49 +239,62 @@ async def 레이드(ctx, *, message=None):
         view4.add_item(button10)
 
         global raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4,rd1,rd2,rd3,msg,msg1,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4
-    if ctx.message.author.display_name == "숯미남" or ctx.message.author.guild_permissions.administrator:
-        if raidcount == 0:
-                    mssg1=message
-                    abc = Embed(title="1번 레이드 길드 팟", description=mssg1)
-                    abc.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
-                    abc.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
-                    msg = await ctx.send(embed=abc,view=view)
-                    raidcount += 1
-        if raidcount == 1:
-                    mssg2=message
-                    abc1 = Embed(title='2번 레이드 길드 팟',description=mssg2)
-                    abc1.add_field(name="1 파티", value=x1[0]+"\n"+x1[1]+"\n"+x1[2]+"\n"+x1[3]+"\n", inline=True)
-                    abc1.add_field(name="2 파티", value=y1[0]+"\n"+y1[1]+"\n"+y1[2]+"\n"+y1[3]+"\n", inline=True)
-                    msg1 = await ctx.send(embed=abc1,view=view1)
-                    raidcount += 1
-        if raidcount == 2:
-                    mssg3=message
-                    abc2 = Embed(title='3번 레이드 길드 팟',description=mssg3)
-                    abc2.add_field(name="1 파티", value=x2[0]+"\n"+x2[1]+"\n"+x2[2]+"\n"+x2[3]+"\n", inline=True)
-                    abc2.add_field(name="2 파티", value=y2[0]+"\n"+y2[1]+"\n"+y2[2]+"\n"+y2[3]+"\n", inline=True)
-                    msg2 = await ctx.send(embed=abc2,view=view2)
-                    raidcount += 1
-        if raidcount == 3:
-                    mssg4=message
-                    abc3 = Embed(title='4번 레이드 길드 팟',description=mssg4)
-                    abc3.add_field(name="1 파티", value=x3[0]+"\n"+x3[1]+"\n"+x3[2]+"\n"+x3[3]+"\n", inline=True)
-                    abc3.add_field(name="2 파티", value=y3[0]+"\n"+y3[1]+"\n"+y3[2]+"\n"+y3[3]+"\n", inline=True)
-                    msg3 = await ctx.send(embed=abc3,view=view3)
-                    raidcount += 1
-        if raidcount == 4:
-                    mssg5=message
-                    abc4 = Embed(title='5번 레이드 길드 팟',description=mssg5)
-                    abc4.add_field(name="1 파티", value=x4[0]+"\n"+x4[1]+"\n"+x4[2]+"\n"+x4[3]+"\n", inline=True)
-                    abc4.add_field(name="2 파티", value=y4[0]+"\n"+y4[1]+"\n"+y4[2]+"\n"+y4[3]+"\n", inline=True)
-                    msg4 = await ctx.send(embed=abc4,view=view4)
-                    raidcount += 1
-        if raidcount == 5:
-                    await ctx.send("풀파티입니다.")
-    else:
-        ctx.send("임원이 아닙니다.")
+        if ctx.message.author.display_name == "숯미남" or ctx.message.author.guild_permissions.administrator:
+            if message == "초기화":
+                x = ["X","X","X","X","X"]
+                y = ["X","X","X","X","X"]
+                x1 = ["X","X","X","X","X"]
+                y1 = ["X","X","X","X","X"]
+                x2 = ["X","X","X","X","X"]
+                y2 = ["X","X","X","X","X"]
+                x3 = ["X","X","X","X","X"]
+                y3 = ["X","X","X","X","X"]
+                x4 = ["X","X","X","X","X"]
+                y4 = ["X","X","X","X","X"]
+                raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4 = 0,0,0,0,0,0,0,0,0,0,0
+                rd1,rd2,rd3,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4="","","","","","","","","","","",""
+            if raidcount == 0:
+                        mssg1=message
+                        abc = Embed(title="1번 레이드 길드 팟", description=mssg1)
+                        abc.add_field(name="1 파티", value=x[0]+"\n"+x[1]+"\n"+x[2]+"\n"+x[3]+"\n", inline=True)
+                        abc.add_field(name="2 파티", value=y[0]+"\n"+y[1]+"\n"+y[2]+"\n"+y[3]+"\n", inline=True)
+                        msg = await ctx.send(embed=abc,view=view)
+                        raidcount += 1
+            if raidcount == 1:
+                        mssg2=message
+                        abc1 = Embed(title='2번 레이드 길드 팟',description=mssg2)
+                        abc1.add_field(name="1 파티", value=x1[0]+"\n"+x1[1]+"\n"+x1[2]+"\n"+x1[3]+"\n", inline=True)
+                        abc1.add_field(name="2 파티", value=y1[0]+"\n"+y1[1]+"\n"+y1[2]+"\n"+y1[3]+"\n", inline=True)
+                        msg1 = await ctx.send(embed=abc1,view=view1)
+                        raidcount += 1
+            if raidcount == 2:
+                        mssg3=message
+                        abc2 = Embed(title='3번 레이드 길드 팟',description=mssg3)
+                        abc2.add_field(name="1 파티", value=x2[0]+"\n"+x2[1]+"\n"+x2[2]+"\n"+x2[3]+"\n", inline=True)
+                        abc2.add_field(name="2 파티", value=y2[0]+"\n"+y2[1]+"\n"+y2[2]+"\n"+y2[3]+"\n", inline=True)
+                        msg2 = await ctx.send(embed=abc2,view=view2)
+                        raidcount += 1
+            if raidcount == 3:
+                        mssg4=message
+                        abc3 = Embed(title='4번 레이드 길드 팟',description=mssg4)
+                        abc3.add_field(name="1 파티", value=x3[0]+"\n"+x3[1]+"\n"+x3[2]+"\n"+x3[3]+"\n", inline=True)
+                        abc3.add_field(name="2 파티", value=y3[0]+"\n"+y3[1]+"\n"+y3[2]+"\n"+y3[3]+"\n", inline=True)
+                        msg3 = await ctx.send(embed=abc3,view=view3)
+                        raidcount += 1
+            if raidcount == 4:
+                        mssg5=message
+                        abc4 = Embed(title='5번 레이드 길드 팟',description=mssg5)
+                        abc4.add_field(name="1 파티", value=x4[0]+"\n"+x4[1]+"\n"+x4[2]+"\n"+x4[3]+"\n", inline=True)
+                        abc4.add_field(name="2 파티", value=y4[0]+"\n"+y4[1]+"\n"+y4[2]+"\n"+y4[3]+"\n", inline=True)
+                        msg4 = await ctx.send(embed=abc4,view=view4)
+                        raidcount += 1
+            if raidcount == 5:
+                        await ctx.send("풀파티입니다.")
+        else:
+            ctx.send("임원이 아닙니다.")
 @bot.command()
 async def 레이드수정(ctx, *, raidedit=None):
-    global raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4,rd1,rd2,rd3,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4
+    global raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4,rd1,rd2,rd3,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4,rd1,rd2,rd3
     if "1번" in raidedit:
         mssg1= re.sub("1번 ","",raidedit)
         rd1 = Embed(title="1번 레이드 길드 팟", description=mssg1)
