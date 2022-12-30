e92dcf9d9eddd1f51ea9218d02022b71f625cc93
LEEv31 <121157749+LEEv31@users.noreply.github.com>
2022-12-30 13:32:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,8 @@ y3 = ["X","X","X","X","X"]
 x4 = ["X","X","X","X","X"]
 y4 = ["X","X","X","X","X"]
 
+role = discord.utils.get(reaction.message.guild.roles, name="")
+
 raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4 = 0,0,0,0,0,0,0,0,0,0,0
 rd1,rd2,rd3,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4,mssg5="","","","","","","","","","","","",""
         
@@ -239,7 +241,7 @@ async def 레이드(ctx, *, message=None):
     view4.add_item(button9)
     view4.add_item(button10)
 
-    if message.guild.get_role(1058238078734565486):
+    if role in user.roles:
             if raidcount == 0:
                         mssg1=message
                         abc = Embed(title="1번 레이드 길드 팟", description=mssg1)
@@ -278,7 +280,7 @@ async def 레이드(ctx, *, message=None):
             if raidcount == 5:
                         await ctx.send("풀파티입니다.")
     else:
-        await ctx.send("임원이 아닙니다.")
+        await ctx.send("공머장이 아닙니다.")
 @bot.command()
 async def 레이드수정(ctx, *, raidedit=None):
     global raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4,rd1,rd2,rd3,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4,mssg5,rd1,rd2,rd3
