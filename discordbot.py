c15ff9cfc2b1fd3294937fda7ea6cba20971914a
LEEv31 <121157749+LEEv31@users.noreply.github.com>
2023-01-01 21:06:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -41,7 +41,7 @@ y4 = ["X","X","X","X","X"]
 raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4 = 0,0,0,0,0,0,0,0,0,0,0
 rd1,rd2,rd3,rd4,rd5,msg,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4,mssg5="","","","","","","","","","","","","","",""
 
-@tree.command(name = "레이드", description="/레이드 일정")
+@tree.command(name = "레이드", description="레이드 일정을 추가합니다.")
 async def slash2(interaction: discord.Interaction, 난이도: str, 군단장: str, 숙련: str, 일정: str):
     global raidcount,i,k,i1,k1,i2,k2,i3,k3,i4,k4,rd1,rd2,rd3,rd4,rd5,msg,msg1,msg2,msg3,msg4,mssg1,mssg2,mssg3,mssg3,mssg4,mssg5
     button1 = Button(label="파티",emoji="1️⃣")
